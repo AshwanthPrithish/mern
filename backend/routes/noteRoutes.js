56375c32dfc9d176194f473654eaf627c208d23a
Ashwanth Prithish <ashwanthprithishcse@gmail.com>
2023-03-22 00:03:11 +0530

    Implemented CRUD functionalities for notes

---
+++ backend/routes/noteRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const {
+  getNotes,
+  CreateNote,
+  getNoteById,
+  UpdateNote,
+  DeleteNote,
+} = require("../controllers/noteController.js");
+const router = express.Router();
+const { protect } = require("../middlewares/authMiddleware.js");
+
+router.route("/").get(protect, getNotes);
+
+router.route("/create").post(protect, CreateNote);
+
+router
+  .route("/:id")
+  .get(getNoteById)
+  .put(protect, UpdateNote)
+  .delete(protect, DeleteNote);
+
+module.exports = router;
